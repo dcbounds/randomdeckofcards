c766526d338a2006a0c24e57b527771f71ddc0a9
dcbounds <boundsdarius@gmail.com>
2019-06-04 04:51:33 +0000

    added old code to js index

--- src/js/index.js
+++ src/js/index.js
@@ -7,5 +7,29 @@ import "../assets/img/4geeks.ico";
 import "../style/index.scss";
 
 window.onload = function() {
+  document.getElementById("the-excuse").innerHTML = generateExcuse();
   console.log("Hello Rigo from the console!");
+};
+
+let generateExcuse = function() {
+  let who = ["The neighbors dog", "R2-D2", "My Autobot", "My pet llama"];
+
+  let what = [
+    "went number 2",
+    "entered The Matrix",
+    "sat on my pinky toe",
+    "leaked poisonous acid"
+  ];
+  let where = [
+    "at church...",
+    "in the shower...",
+    "at the playgrond...",
+    "at the library..."
+  ];
+
+  let whoIndex = Math.floor(Math.random() * who.length);
+  let whatIndex = Math.floor(Math.random() * what.length);
+  let whereIndex = Math.floor(Math.random() * where.length);
+
+  return who[whoIndex] + " " + what[whatIndex] + " " + where[whereIndex];
 };
